196205aeba754085e5409853b300ea98df212a49
Peter Ke <peterke0911@gmail.com>
2024-10-23 16:55:15 -0700

    do not split files in compaction

--- crates/core/src/operations/optimize.rs
+++ crates/core/src/operations/optimize.rs
@@ -451,6 +451,7 @@ impl MergePlan {
         files: MergeBin,
         object_store: ObjectStoreRef,
         read_stream: F,
+        should_split_files: bool,
     ) -> Result<(Vec<Action>, PartialMetrics), DeltaTableError>
     where
         F: Future<Output = Result<ParquetReadStream, DeltaTableError>> + Send + 'static,
@@ -491,7 +492,7 @@ impl MergePlan {
             task_parameters.file_schema.clone(),
             partition_values.clone(),
             Some(task_parameters.writer_properties.clone()),
-            Some(task_parameters.input_parameters.target_size as usize),
+            should_split_files.then_some(task_parameters.input_parameters.target_size as usize),
             None,
         )?;
         let mut writer = PartitionWriter::try_with_config(
@@ -713,6 +714,9 @@ impl MergePlan {
                         files,
                         log_store.object_store().clone(),
                         futures::future::ready(Ok(batch_stream)),
+                        // Do not split the output into multiple files because files were already
+                        // grouped by size
+                        false,
                     ));
                     util::flatten_join_error(rewrite_result)
                 })
@@ -749,6 +753,7 @@ impl MergePlan {
                             files,
                             log_store.object_store(),
                             batch_stream,
+                            true,
                         ));
                         util::flatten_join_error(rewrite_result)
                     })
